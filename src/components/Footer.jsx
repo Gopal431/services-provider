fa877f5ed2e8c40c9089a9717543c12adc969a88
Gopal431 <prasadgopal431@gmail.com>
2022-11-02 01:10:23 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>PrasadGopal</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.linkedin.com/in/gopal-prasad/" target={"blank"}>
+            Linkedin
+          </a>
+          <a href="https://www.instagram.com/prasadgopal431/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/Gopal431" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
